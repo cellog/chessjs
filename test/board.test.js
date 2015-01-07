a9148b073e05ae2ce83a6029cbcc5a2fdd4d027d
Gregory Beaver <greg@chiaraquartet.net>
2015-01-07 14:44:22 -0600

    fix exports
    1 passing test, now to make the second test pass

--- test/board.test.js
+++ test/board.test.js
@@ -2,20 +2,27 @@ var should = require("should"),
     chess = require("../lib/board.js")
 
 describe("Chessboard", function() {
+    var myboard;
+    
+    before(function() {
+        myboard = new chess.board;
+    });
+
     it("should have 64 squares", function() {
-        chess.squares.should.be.an.instanceof(Array).and.have.length.exactly(64)
+        myboard.squares.should.be.an.instanceof(Array)
+        myboard.squares.length.should.eql(64)
     })
 
     it("should translate coords to array index", function() {
-        chess.indexOf('a1').should.eql(0)
-        chess.indexOf('h8').should.eql(64)
-        chess.indexOf('a8').should.eql(7)
-        chess.indexOf('b1').should.eql(8+0)
-        chess.indexOf('c1').should.eql(8*2+0)
-        chess.indexOf('d1').should.eql(8*3+0)
-        chess.indexOf('e1').should.eql(8*4+0)
-        chess.indexOf('f1').should.eql(8*5+0)
-        chess.indexOf('g1').should.eql(8*6+0)
-        chess.indexOf('h1').should.eql(8*7+0)
+        myboard.indexOf('a1').should.eql(0)
+        myboard.indexOf('h8').should.eql(64)
+        myboard.indexOf('a8').should.eql(7)
+        myboard.indexOf('b1').should.eql(8+0)
+        myboard.indexOf('c1').should.eql(8*2+0)
+        myboard.indexOf('d1').should.eql(8*3+0)
+        myboard.indexOf('e1').should.eql(8*4+0)
+        myboard.indexOf('f1').should.eql(8*5+0)
+        myboard.indexOf('g1').should.eql(8*6+0)
+        myboard.indexOf('h1').should.eql(8*7+0)
     });
 })
