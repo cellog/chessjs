a878108ab8ce40f13b284dcb54cbcb6bbdcbe9a3
Gregory Beaver <greg@chiaraquartet.net>
2015-01-07 13:46:15 -0600

    add a new failing test

--- test/board.test.js
+++ test/board.test.js
@@ -3,6 +3,19 @@ var should = require("should"),
 
 describe("Chessboard", function() {
     it("should have 64 squares", function() {
-        chess.squares.should.be.an.instanceof(Array).and.have.length.exactly(64);
+        chess.squares.should.be.an.instanceof(Array).and.have.length.exactly(64)
     })
+
+    it("should translate coords to array index", function() {
+        chess.indexOf('a1').should.eql(0)
+        chess.indexOf('h8').should.eql(64)
+        chess.indexOf('a8').should.eql(7)
+        chess.indexOf('b1').should.eql(8+0)
+        chess.indexOf('c1').should.eql(8*2+0)
+        chess.indexOf('d1').should.eql(8*3+0)
+        chess.indexOf('e1').should.eql(8*4+0)
+        chess.indexOf('f1').should.eql(8*5+0)
+        chess.indexOf('g1').should.eql(8*6+0)
+        chess.indexOf('h1').should.eql(8*7+0)
+    });
 })
