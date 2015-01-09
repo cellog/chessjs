7b6ccf111d2c523660325529e7bb0527ca8c31f6
Gregory Beaver <greg@chiaraquartet.net>
2015-01-08 22:43:36 -0600

    not working knight jump calculation, save work for now

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var chess = require('./lib/board.js')
 var bit = chess.bitboard.fromBinary(
-                    '00000000' +
-                    '01100000' +
+                    '00000001' +
+                    '00100000' +
                     '00000100' +
-                    '00000010' +
-                    '00000011' +
-                    '00011000' +
-                    '11000000' +
+                    '00000000' +
+                    '00000000' +
+                    '00010000' +
+                    '10000000' +
                     '00000010'
 )
 console.log(bit.debugString("\n"), "\n")
-console.log(bit.northwestOne().debugString("\n"))
+console.log(bit.knightJumps().debugString("\n"))
