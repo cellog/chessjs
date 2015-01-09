8cf2a128d8b6aa1eb478154ec47c363717932a97
Gregory Beaver <greg@chiaraquartet.net>
2015-01-09 16:27:20 -0600

    refactor to a mini-long, implement king moves

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var chess = require('./lib/board.js')
 
 var bit = chess.bitboard.fromBinary(
-                    '10000001' +
                     '00000000' +
                     '00000000' +
-                    '00010000' +
-                    '10000000' +
                     '00000000' +
                     '00000000' +
-                    '10000001'
+                    '00000000' +
+                    '00000000' +
+                    '00000000' +
+                    '10000000'
 )
 console.log(bit.debugString("\n"), "\n")
-console.log(bit.knightJumps().debugString("\n"))
+console.log(bit.kingMoves().debugString("\n"))
--- test/board.test.js
+++ test/board.test.js
@@ -1114,5 +1114,71 @@ describe("Bitboard", function() {
                     '00010100'
                 )
         })
-    })
+    }) // knight moves
+    
+    describe("king moves", function() {
+        it("should have correct corners", function() {
+            chess.bitboard.fromBinary(
+                    '10000001' +
+                    '00000000' +
+                    '00000000' +
+                    '00000000' +
+                    '00000000' +
+                    '00000000' +
+                    '00000000' +
+                    '10000001'
+                ).kingMoves().debugString("\n").should.eql(
+                    '01000010' + "\n" +
+                    '11000011' + "\n" +
+                    '00000000' + "\n" +
+                    '00000000' + "\n" +
+                    '00000000' + "\n" +
+                    '00000000' + "\n" +
+                    '11000011' + "\n" +
+                    '01000010'
+                )
+        })
+        it("should have correct sides", function() {
+            chess.bitboard.fromBinary(
+                    '00001000' +
+                    '00000000' +
+                    '00000000' +
+                    '10000000' +
+                    '00000001' +
+                    '00000000' +
+                    '00000000' +
+                    '00010000'
+                ).kingMoves().debugString("\n").should.eql(
+                    '00010100' + "\n" +
+                    '00011100' + "\n" +
+                    '11000000' + "\n" +
+                    '01000011' + "\n" +
+                    '11000010' + "\n" +
+                    '00000011' + "\n" +
+                    '00111000' + "\n" +
+                    '00101000'
+                )
+        })
+        it("should have correct centers", function() {
+            chess.bitboard.fromBinary(
+                    '00000000' +
+                    '00000000' +
+                    '00010000' +
+                    '00000000' +
+                    '00000000' +
+                    '00001000' +
+                    '00000000' +
+                    '00000000'
+                ).kingMoves().debugString("\n").should.eql(
+                    '00000000' + "\n" +
+                    '00111000' + "\n" +
+                    '00101000' + "\n" +
+                    '00111000' + "\n" +
+                    '00011100' + "\n" +
+                    '00010100' + "\n" +
+                    '00011100' + "\n" +
+                    '00000000'
+                )
+        })
+    }) // king moves
 }) // Bitboard
