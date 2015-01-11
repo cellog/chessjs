8955e15d04e7f4748b069776c78a59eefdc5d1ef
Gregory Beaver <greg@chiaraquartet.net>
2015-01-11 00:36:15 -0600

    implement bit scanning
    based on deBruijn algorithm
    (http://supertech.csail.mit.edu/papers/debruijn.pdf) for detecting the
    least significant bit index, this is the most efficient algorithm
    possible in javascript due to its 32-bit limitations.  It works
    fabulously!

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
 var chess = require('./lib/board.js')
+var bit = new chess.bitboard(0xffffffff,0xffffffff)
 
-var board = new chess.board()
-            board.bitboards.p = chess.bitboard.fromBinary(
-                '00000000' +
-                '10011111' +
-                '01000000' +
-                '00000000' +
-                '01000000' +
-                '00000000' +
-                '00000000' +
-                '00000000'
-            )
-            board.bitboards.P = chess.bitboard.fromBinary(
-                '00000000' +
-                '00000000' +
-                '00000000' +
-                '10000000' +
-                '00000000' +
-                '00000000' +
-                '01110110' +
-                '00000000'
-            )
-
-
-console.log(board.bitboardAttacks(true).debugString("\n"), "\n")
+console.log(bit.debugString("\n"))
+console.log(bit.bitscan())
--- test/bitboard.test.js
+++ test/bitboard.test.js
@@ -1251,4 +1251,65 @@ describe("Bitboard", function() {
             })
         }) // attack bitboards
     }) // sliding piece moves
+
+    describe("least significant bit detection", function() {
+        before(function() {
+            bit = new chess.bitboard(0,0)
+        })
+        it("should return 64 for empty bitboard", function() {
+            bit.lsb().should.eql(64)
+        })
+        it("should detect lowest bits", function() {
+            var bit = new chess.bitboard(0,0)
+            for (var i = 0;i < 31;i++) {
+                it("test " + i, function() {
+                    bit.l = 1 << i
+                    bit.lsb().should.eql(i)
+                })
+            }
+            it("test 31", function() {
+                bit.l = 1 << 31
+                bit.lsb().should.eql(31)
+            })
+        })
+        it("should detect highest bits", function() {
+            for (var i = 0;i < 31;i++) {
+                it("test " + i, function() {
+                    bit.h = 1 << i
+                    bit.lsb().should.eql(i+32)
+                })
+            }
+            it("test 31", function() {
+                bit.h = 1 << 31
+                bit.lsb().should.eql(63)
+            })
+        })
+    })
+    describe("bitscan", function() {
+        var bit = new chess.bitboard(0,0)
+        it("detect all the bits are belong to us", function() {
+            var allthebasearebelongtous = []
+            for (var i=0;i<64;i++) allthebasearebelongtous.push(i)
+            bit.l = bit.h = 0xffffffff
+            bit.bitscan().should.eql(allthebasearebelongtous)
+        })
+        it("detect bits with zeros and stuff", function() {
+            var bit = chess.bitboard.fromBinary(
+                '00001000' +
+                '00001000' +
+                '00001000' +
+                '11111111' +
+                '00001000' +
+                '00001000' +
+                '00001000' +
+                '11111111'
+            )
+            bit.bitscan().should.eql([3,11,19,24,25,26,27,28,29,30,31,
+                                      35,43,51,56,57,58,59,60,61,62,63])
+        })
+        it("zero returns []", function() {
+            bit.h = bit.l = 0
+            bit.bitscan().should.eql([])
+        })
+    })
 }) // Bitboard
