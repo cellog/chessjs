276a13172a6fc2b77d188d1142462d73964e7363
Gregory Beaver <greg@chiaraquartet.net>
2015-01-26 21:15:27 -0600

    getConnectingRay() works now

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 var chess = require('./lib/board.js')
-var myboard = chess.board.fromTextBoard(
-                '....B...' +
-                '........' +
-                '...k....' +
-                '....B...' +
-                '........' +
-                '....b...' +
-                '.....K..' +
-                '........'
-            )
-            
-//console.log(myboard.blackpieces.debugString("\n"),"\n")
-//console.log(myboard.bitboards.k.orB(myboard.bitboards.P).debugString("\n"),"\n")
-//console.log("attacking\n",myboard.bitboards.black[0].pawnAttackTargets(false).debugString("\n"),"\n")
-//myboard.blackToMove()
-console.log(myboard.getPotentialKingMoves().debugString("\n"))
+console.log(chess.bitboard.fromBinary(
+    '00000000' +
+    '00000000' +
+    '00000000' +
+    '00000000' +
+    '00010000' +
+    '00000000' +
+    '00000000' +
+    '00000000'
+).getConnectingRay(chess.bitboard.fromBinary(
+    '00000000' +
+    '00000000' +
+    '00000000' +
+    '00000000' +
+    '00000000' +
+    '00000000' +
+    '00000000' +
+    '00000010'
+), 'bishop').debugString("\n"))
--- test/bitboard/getconnectingray.test.js
+++ test/bitboard/getconnectingray.test.js
@@ -372,16 +372,335 @@ chess.bitboard.fromBinary(
         })
     })
     describe("diagonal intersections", function() {
-        it("bishop should return a diagonal ray southeast")
-        it("queen should return a diagonal ray southeast")
-        it("bishop should return a diagonal ray southwest")
-        it("queen should return a diagonal ray southwest")
-        it("bishop should return a diagonal ray northwest")
-        it("queen should return a diagonal ray northwest")
-        it("bishop should return a diagonal ray northeast")
-        it("queen should return a diagonal ray northeast")
-        it("rook should return empty")
-        it("bishop with no intersection should return empty")
-        it("queen with no intersection should return empty")
+        it("bishop should return a diagonal ray southeast", function() {
+            chess.bitboard.fromBinary(
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00010000' +
+                '00000000' +
+                '00000000' +
+                '00000000'
+            ).getConnectingRay(chess.bitboard.fromBinary(
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000010'
+            ), 'bishop').debugString("\n").should.eql(
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00001000' + "\n" +
+                '00000100' + "\n" +
+                '00000010'
+            )
+        })
+        it("queen should return a diagonal ray southeast", function() {
+            chess.bitboard.fromBinary(
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00010000' +
+                '00000000' +
+                '00000000' +
+                '00000000'
+            ).getConnectingRay(chess.bitboard.fromBinary(
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000010'
+            ), 'queen').debugString("\n").should.eql(
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00001000' + "\n" +
+                '00000100' + "\n" +
+                '00000010'
+            )
+        })
+        it("bishop should return a diagonal ray southwest", function() {
+            chess.bitboard.fromBinary(
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00010000' +
+                '00000000' +
+                '00000000' +
+                '00000000'
+            ).getConnectingRay(chess.bitboard.fromBinary(
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '10000000'
+            ), 'bishop').debugString("\n").should.eql(
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00100000' + "\n" +
+                '01000000' + "\n" +
+                '10000000'
+            )
+        })
+        it("queen should return a diagonal ray southwest", function() {
+            chess.bitboard.fromBinary(
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00010000' +
+                '00000000' +
+                '00000000' +
+                '00000000'
+            ).getConnectingRay(chess.bitboard.fromBinary(
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '10000000'
+            ), 'queen').debugString("\n").should.eql(
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00100000' + "\n" +
+                '01000000' + "\n" +
+                '10000000'
+            )
+        })
+        it("bishop should return a diagonal ray northwest", function() {
+            chess.bitboard.fromBinary(
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00010000' +
+                '00000000' +
+                '00000000' +
+                '00000000'
+            ).getConnectingRay(chess.bitboard.fromBinary(
+                '00000000' +
+                '10000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000'
+            ), 'bishop').debugString("\n").should.eql(
+                '00000000' + "\n" +
+                '10000000' + "\n" +
+                '01000000' + "\n" +
+                '00100000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000'
+            )
+        })
+        it("queen should return a diagonal ray northwest", function() {
+            chess.bitboard.fromBinary(
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00010000' +
+                '00000000' +
+                '00000000' +
+                '00000000'
+            ).getConnectingRay(chess.bitboard.fromBinary(
+                '00000000' +
+                '10000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000'
+            ), 'queen').debugString("\n").should.eql(
+                '00000000' + "\n" +
+                '10000000' + "\n" +
+                '01000000' + "\n" +
+                '00100000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000'
+            )
+        })
+        it("bishop should return a diagonal ray northeast", function() {
+            chess.bitboard.fromBinary(
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00010000' +
+                '00000000' +
+                '00000000' +
+                '00000000'
+            ).getConnectingRay(chess.bitboard.fromBinary(
+                '00000001' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000'
+            ), 'bishop').debugString("\n").should.eql(
+                '00000001' + "\n" +
+                '00000010' + "\n" +
+                '00000100' + "\n" +
+                '00001000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000'
+            )
+        })
+        it("queen should return a diagonal ray northeast", function() {
+            chess.bitboard.fromBinary(
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00010000' +
+                '00000000' +
+                '00000000' +
+                '00000000'
+            ).getConnectingRay(chess.bitboard.fromBinary(
+                '00000001' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000'
+            ), 'queen').debugString("\n").should.eql(
+                '00000001' + "\n" +
+                '00000010' + "\n" +
+                '00000100' + "\n" +
+                '00001000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000'
+            )
+        })
+        it("rook should return empty", function() {
+            chess.bitboard.fromBinary(
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00010000' +
+                '00000000' +
+                '00000000' +
+                '00000000'
+            ).getConnectingRay(chess.bitboard.fromBinary(
+                '00000001' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000'
+            ), 'rook').debugString("\n").should.eql(
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000'
+            )
+        })
+        it("bishop with no intersection should return empty", function() {
+            chess.bitboard.fromBinary(
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00010000' +
+                '00000000' +
+                '00000000' +
+                '00000000'
+            ).getConnectingRay(chess.bitboard.fromBinary(
+                '00000010' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000'
+            ), 'bishop').debugString("\n").should.eql(
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000'
+            )
+        })
+        it("queen with no intersection should return empty", function() {
+            chess.bitboard.fromBinary(
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00010000' +
+                '00000000' +
+                '00000000' +
+                '00000000'
+            ).getConnectingRay(chess.bitboard.fromBinary(
+                '00000010' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000' +
+                '00000000'
+            ), 'queen').debugString("\n").should.eql(
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000' + "\n" +
+                '00000000'
+            )
+        })
     })
 }) // Bitboard
