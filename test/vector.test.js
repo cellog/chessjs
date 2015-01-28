0f86bb1e94324e88dbd64ea2859f50076dc82787
Gregory Beaver <greg@chiaraquartet.net>
2015-01-28 08:27:28 -0600

    begin speculative work on extending vector
    untested, mostly

--- test/vector.test.js
+++ test/vector.test.js
@@ -64,31 +64,31 @@ describe("Vector", function() {
         })
     })
 
-    describe("toChessSquare", function() {
+    describe("toAlgebraic", function() {
         it ("should return 'a1' for 0 vector", function () {
-            new Vector(0,0).toChessSquare().should.eql('a1')
+            new Vector(0,0).toAlgebraic().should.eql('a1')
         })
         it ("should return 'b1' for 1,0 vector", function () {
-            new Vector(1,0).toChessSquare().should.eql('b1')
+            new Vector(1,0).toAlgebraic().should.eql('b1')
         })
         it ("should return 'h1' for 7,0 vector", function () {
-            new Vector(7,0).toChessSquare().should.eql('h1')
+            new Vector(7,0).toAlgebraic().should.eql('h1')
         })
         it ("should return 'a2' for 0,1 vector", function () {
-            new Vector(0,1).toChessSquare().should.eql('a2')
+            new Vector(0,1).toAlgebraic().should.eql('a2')
         })
         it ("should return 'h8' for 7,7 vector", function () {
-            new Vector(7,7).toChessSquare().should.eql('h8')
+            new Vector(7,7).toAlgebraic().should.eql('h8')
         })
         it ("should fail on invalid input", function() {
             var z = new Vector(-1,0);
-            z.toChessSquare.bind(z).should.throw("Invalid chess square (-1,0)")
+            z.toAlgebraic.bind(z).should.throw("Invalid chess square (-1,0)")
             z.set(0,-1)
-            z.toChessSquare.bind(z).should.throw("Invalid chess square (0,-1)")
+            z.toAlgebraic.bind(z).should.throw("Invalid chess square (0,-1)")
             z.set(8,0)
-            z.toChessSquare.bind(z).should.throw("Invalid chess square (8,0)")
+            z.toAlgebraic.bind(z).should.throw("Invalid chess square (8,0)")
             z.set(0,8)
-            z.toChessSquare.bind(z).should.throw("Invalid chess square (0,8)")
+            z.toAlgebraic.bind(z).should.throw("Invalid chess square (0,8)")
         })
     })
 
