e9686380dbd1983bf516d566de8fba223fc84f8a
Gregory Beaver <greg@chiaraquartet.net>
2015-02-07 11:38:49 -0600

    cover parseAlgebraicNotation
    fix error in implementation of getConnectingRays:
    we were returning rays that didn’t connect to the target square

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 var chess = require('./lib/board.js'),
     Vector = chess.vector
-var a = chess.bitboard.fromBinary(
-                '00000000' +
-                '00000000' +
-                '00000000' +
-                '00000000' +
-                '00010000' +
-                '00000000' +
-                '00000000' +
-                '00000000'
-            ).getConnectingRays(chess.bitboard.fromBinary(
-                '00010000' +
-                '00000010' +
-                '01000000' +
-                '00000000' +
-                '01000010' +
-                '01000000' +
-                '00010100' +
-                '10000000'
-            ), 'queen', true)
-            a.debugString.bind(a, "oops").call()
+var myboard = chess.board.fromTextBoard(
+                'Q...QQ..' +
+                '....p...' +
+                'p.......' +
+                'Q.......' +
+                '........' +
+                '........' +
+                '....Q...' +
+                '........'
+            )
+console.log(myboard.parseAlgebraicNotation('Qe6'))
---
+++ test/board/parsealgebraicnotation.test.js
@@ -0,0 +1,109 @@
+var should = require("should"),
+    chess = require("../../lib/board.js"),
+    vector = require("../../lib/vector.js").vector
+
+describe("Chessboard parseAlgebraicNotation", function() {
+    var myboard = new chess.board;
+
+    describe("invalid notation", function() {
+        it("Ta4", function() {
+            myboard.parseAlgebraicNotation.bind(myboard, 'Ta4')
+                .should.throwError(chess.InvalidMoveError,
+                                   {message: '"Ta4" is not valid algebraic notation', name: "InvalidMoveError"})
+        })
+        it("Qxy4", function() {
+            myboard.parseAlgebraicNotation.bind(myboard, 'Qxy4')
+                .should.throwError(chess.InvalidMoveError,
+                                   {message: '"Qxy4" is not valid algebraic notation', name: "InvalidMoveError"})
+        })
+        it("Qxa14", function() {
+            myboard.parseAlgebraicNotation.bind(myboard, 'Qxa14')
+                .should.throwError(chess.InvalidMoveError,
+                                   {message: '"Qxa14" is not valid algebraic notation', name: "InvalidMoveError"})
+        })
+        it("Qabc3", function() {
+            myboard.parseAlgebraicNotation.bind(myboard, 'Qabc3')
+                .should.throwError(chess.InvalidMoveError,
+                                   {message: '"Qabc3" is not valid algebraic notation', name: "InvalidMoveError"})
+        })
+        it("abxc4", function() {
+            myboard.parseAlgebraicNotation.bind(myboard, 'abxc4')
+                .should.throwError(chess.InvalidMoveError,
+                                   {message: '"abxc4" is not valid algebraic notation', name: "InvalidMoveError"})
+        })
+    })
+    describe("queen notation", function() {
+        before(function() {
+            myboard = chess.board.fromTextBoard(
+                'Q...QQ..' +
+                '....p...' +
+                'p.......' +
+                'Q.......' +
+                '......p.' +
+                '........' +
+                '....Q...' +
+                '........'
+            )
+        })
+        after(function() {
+            myboard = new chess.board
+        })
+        it("Qe4", function() {
+            myboard.parseAlgebraicNotation('Qe6').should.eql(['e2', 'e6'])
+        })
+        it("Qd1", function() {
+            myboard.parseAlgebraicNotation('Qd1').should.eql(['e2', 'd1'])
+        })
+        it("Qc2", function() {
+            myboard.parseAlgebraicNotation('Qc2').should.eql(['e2', 'c2'])
+        })
+        it("Qh2", function() {
+            myboard.parseAlgebraicNotation('Qh2').should.eql(['e2', 'h2'])
+        })
+        it("Qxg4", function() {
+            myboard.parseAlgebraicNotation('Qxg4').should.eql(['e2', 'g4'])
+        })
+        it("Qe2xa6", function() {
+            myboard.parseAlgebraicNotation('Qe2xa6').should.eql(['e2', 'a6'])
+        })
+        it("Qfxe7", function() {
+            myboard.parseAlgebraicNotation('Qfxe7').should.eql(['f8', 'e7'])
+        })
+        it("Q5xa6", function() {
+            myboard.parseAlgebraicNotation('Q5xa6').should.eql(['a5', 'a6'])
+        })
+        it("Q8xa6", function() {
+            myboard.parseAlgebraicNotation('Q8xa6').should.eql(['a8', 'a6'])
+        })
+        it("invalid: Q8xb5", function() {
+            myboard.parseAlgebraicNotation.bind(myboard, 'Q8xb5')
+                .should.throwError(chess.InvalidMoveError,
+                                   {message: 'Queens cannot move to "b5"', name: "InvalidMoveError"})
+        })
+        it("invalid: Qb5", function() {
+            myboard.parseAlgebraicNotation.bind(myboard, 'Qb5')
+                .should.throwError(chess.InvalidMoveError,
+                                   {message: 'Too many queens can move to "b5", need more disambiguation than "Qb5"', name: "InvalidMoveError"})
+        })
+        it("invalid: Qe8", function() {
+            myboard.parseAlgebraicNotation.bind(myboard, 'Qe8')
+                .should.throwError(chess.InvalidMoveError,
+                                   {message: 'Queens cannot move to "e8"', name: "InvalidMoveError"})
+        })
+        it("invalid: Qxe3", function() {
+            myboard.parseAlgebraicNotation.bind(myboard, 'Qxe3')
+                .should.throwError(chess.InvalidMoveError,
+                                   {message: 'Queens cannot move to "e3"', name: "InvalidMoveError"})
+        })
+        it("invalid: Qxd7", function() {
+            myboard.parseAlgebraicNotation.bind(myboard, 'Qxd7')
+                .should.throwError(chess.InvalidMoveError,
+                                   {message: 'Queens cannot move to "d7"', name: "InvalidMoveError"})
+        })
+        it("invalid: Qa1xa3", function() {
+            myboard.parseAlgebraicNotation.bind(myboard, 'Qa1xa3')
+                .should.throwError(chess.InvalidMoveError,
+                                   {message: 'No queen exists on "a1"', name: "InvalidMoveError"})
+        })
+    })
+})
